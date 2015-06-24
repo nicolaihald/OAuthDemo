a4495700d8c6c3471386e98200cbe2cea760f562
Nicolai Hald <nha@miracleas.dk>
2015-06-24 10:52:55 +0200

    Added nuget OWIN nuget pakages

---
+++ OAuthDemo/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OAuthDemo.Startup))]
+namespace OAuthDemo
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+
+    }
+}
