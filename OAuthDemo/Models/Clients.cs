268e4a4fbcea83cb6e8c05d7e16610be775f1283
Nicolai Hald <nha@miracleas.dk>
2015-06-24 14:25:07 +0200

    Moved Clients to Models namespace

---
+++ OAuthDemo/Models/Clients.cs
@@ -0,0 +1,17 @@
+namespace OAuthDemo.Models
+{
+    public class Clients
+    {
+        public class Client1
+        {
+            public static string Id { get; set; }
+            public static string RedirectUrl { get; set; }
+        }
+
+        public class Client2
+        {
+            public static string Id { get; set; }
+            public static string RedirectUrl { get; set; }
+        }
+    }
+}
--- OAuthDemo/Providers/DemoAuthorizationServerProvider.cs
+++ OAuthDemo/Providers/DemoAuthorizationServerProvider.cs
@@ -187,19 +187,4 @@ namespace OAuthDemo.Providers
 
 
     }
-
-    public class Clients
-    {
-        public class Client1
-        {
-            public static string Id { get; set; }
-            public static string RedirectUrl { get; set; }
-        }
-
-        public class Client2
-        {
-            public static string Id { get; set; }
-            public static string RedirectUrl { get; set; }
-        }
-    }
 }
